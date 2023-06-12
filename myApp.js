eb636b7bc42c6e226f8effedd87dcd42b45f6e89
Jonn Horn <jonnhorn@gmail.com>
2023-06-12 14:35:00 -0700

    added sendFile support

--- myApp.js
+++ myApp.js
@@ -3,9 +3,15 @@ let app = express();
 
 console.log("Hello World");
 
+/*
 app.get('/', (req, res) => {
     res.send('Hello Express');
 }) ;
+*/
 
+const sendPath = __dirname + '/views/index.html';
+app.get('/', (req, res) => {
+    res.sendFile(sendPath);
+}) ;
 
  module.exports = app;
