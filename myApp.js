aebce23a81622773ae032d92226649718b82a00f
Jonn Horn <jonnhorn@gmail.com>
2023-06-12 22:34:39 -0700

    added console log for router

--- myApp.js
+++ myApp.js
@@ -12,10 +12,20 @@ let msg_json = 'Hello json';
 
 app.use('/public', express.static(staticPath));
 
+app.use((req, res, next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+})
+
 app.get('/json', (req, res) => {
     console.log("MESSAGE_STYLE: " + process.env.MESSAGE_STYLE);
-    if(process.env.MESSAGE_STYLE === 'uppercase') msg_json = msg_json.toUpperCase();
-    res.json( { "message": msg_json });
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json( { "message": "Hello json".toUpperCase() });
+    }
+    else {
+        res.json( { "message": "Hello json" });
+    }
+    
 })
 
 app.get('/', (req, res) => {
