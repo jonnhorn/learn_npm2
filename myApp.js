cc7751f3621fb8600e29dbd422bc5d6be272a401
Jonn Horn <jonnhorn@gmail.com>
2023-06-14 23:22:53 -0700

    added body content support

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 console.log("Hello World");
 console.log("MESSAGE_STYLE: " + process.env.MESSAGE_STYLE);
@@ -12,26 +13,38 @@ let msg_json = 'Hello json';
 
 app.use('/public', express.static(staticPath));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+// log the incoming requests
 app.use((req, res, next) => {
     console.log(req.method + ' ' + req.path + ' - ' + req.ip);
     next();
 })
 
+//return the curr time for path /now
 app.get('/now', function(req, res, next) {
     req.time = new Date().toString();
     next();
 }, function(req, res) {
    res.json({ "time": req.time });
-   //res.json({ "time": "good times" })
 });
 
+// echo based on path
 app.get('/:word/echo', function(req, res, next) {
     res.json({'echo': req.params.word })
 })
+
+// return URL params 
 app.get('/name', function(req, res, next) {
     res.json({ 'name': req.query.first + ' ' + req.query.last });
 })
 
+// return body content
+app.post('/name', function(req,res,next) {
+    res.json({ 'name': req.body.first + ' ' + req.body.last });
+})
+
+// use an ENV variable to modify response
 app.get('/json', (req, res) => {
     console.log("MESSAGE_STYLE: " + process.env.MESSAGE_STYLE);
     if(process.env.MESSAGE_STYLE === 'uppercase') {
@@ -43,6 +56,7 @@ app.get('/json', (req, res) => {
     
 })
 
+// respond to root GET
 app.get('/', (req, res) => {
     res.sendFile(sendPath);
 }) ;
